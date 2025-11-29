84bf202f72d383ffb96c1f1c2840c145e69a0f39
Wronker99 <f.krappmann@googlemail.com>
2025-11-29 16:57:21 +0100

    Add files via upload

--- Projekt_1.py
+++ Projekt_1.py
@@ -8,7 +8,8 @@ import random
 import collections
 
 #Allgemeine Startbedingungen
-spielkarten = [200, 300, 400, 500, 600, "x2", "Aussetzer"]
+spielkarten = [200, 300, 400, 500, 600, "x2", "Aussetzer", "Strasse"]
+#spielkarten = ["Strasse"]
 
 def spieler_eingabe():
     spielername = -1
@@ -28,7 +29,6 @@ def spieler_eingabe():
                 spieler.append(spielername)
     return spieler
             
-
 def wuerfeln(anzahl_wuerfel):
     wurf = [random.randint(1,6) for _ in range(anzahl_wuerfel)]
     wurf.sort()
@@ -78,13 +78,12 @@ def punkte_pruefen(wurf):
             korrekte_Auswahl = False
     return punkte, korrekte_Auswahl
 
-
 def spielkarte_bonus(bonus): #letzer Würfel muss auch "bewusst" gewählt werden, vll ändern 
     anzahl_wuerfel = 6
     punkte = 0
     nochmal_wuerfeln = "ja"
     tutto = False
-    while nochmal_wuerfeln != "nein" and anzahl_wuerfel != 0:
+    while nochmal_wuerfeln != "nein" and anzahl_wuerfel > 0:
         wurf=wuerfeln(anzahl_wuerfel)
         if punkte_pruefen(wurf)[0] == 0:
             print("\nIhr Wurf: ", *wurf,"\nKeine Punkte. Zug beendet.\n")
@@ -110,11 +109,34 @@ def spielkarte_bonus(bonus): #letzer Würfel muss auch "bewusst" gewählt werden
                 punkte += bonus
             tutto = True
         else:
-            print("\nVor Ihnen liegen akutell", punkte, "Punkte.")
-            nochmal_wuerfeln = input("\nNochmal würfeln [ja] oder Zug beenden [nein]: \n")
+            print(f"\nVor Ihnen liegen akutell {punkte} Punkte.\n")
+            print(f"\nNochmal mit {anzahl_wuerfel} Würfel würfeln [ja] oder Zug beenden [nein]: \n")
+            nochmal_wuerfeln = input()
     return punkte, tutto
 
+def spielkarte_strasse():
+    anzahl_wuerfel = 6
+    strasse = []
+    weitermachen = True
+    while weitermachen == True and anzahl_wuerfel > 0:
+        wurf = wuerfeln(anzahl_wuerfel)
+        weitermachen = False
+        for wuerfel in wurf:
+            if wuerfel not in strasse:
+                strasse.append(wuerfel)
+                anzahl_wuerfel -= 1
+                weitermachen = True
                 
+    strasse.sort()
+    if len(strasse) == 6:
+        tutto = True
+        punkte_karte = 2000
+    else:
+        tutto = False
+        punkte_karte = 0
+    return punkte_karte, tutto    
+
+
 def spielerzug(): #Wenn 1. Wurf ein Nullwurf, Ausgabe "Alle Punkte verloren"
     punkte_runde = 0
     while True:
@@ -125,14 +147,19 @@ def spielerzug(): #Wenn 1. Wurf ein Nullwurf, Ausgabe "Alle Punkte verloren"
             punkte_karte, tutto = spielkarte_bonus(gezogene_karte)
         elif gezogene_karte == "Aussetzer":
             punkte_runde = 0
-            print("\nSie müssen Aussetzen.\n")
+            print("\nSie müssen Aussetzen. Punkte aus dieser Runde wurden verloren.\n")
             return punkte_runde
+        elif gezogene_karte == "Strasse":
+            print("\n Sie haben eine Strasse gezogen\n")
+            punkte_karte, tutto = spielkarte_strasse()
+            
         #Hier weitere karten einfügen
+        
         if punkte_karte > 0:
             punkte_runde += punkte_karte
         else:
             punkte_runde = 0
-            print("\nAlle Punkte verloren.\n") 
+            print("\nKeine Punkte für diese Runde.\n") 
         if tutto == False:
             return punkte_runde
         else:
@@ -142,7 +169,6 @@ def spielerzug(): #Wenn 1. Wurf ein Nullwurf, Ausgabe "Alle Punkte verloren"
             else:
                 return punkte_runde
 
-
 def spielablauf():
     spieler = spieler_eingabe() #Beim Erreichen der Punkte, wird Spiel direkt beendet.
     spielmarker = random.randint(0,len(spieler)+1)
@@ -150,7 +176,7 @@ def spielablauf():
     spiel_beendet = False
     while not spiel_beendet:
         for spieler_name, punkte in punktestand.items():
-            if punkte > 6000:
+            if punkte >= 6000:
                 print(f"\n{spieler_name}, Sie haben gewonnen und haben {punkte} Punkte.\n")
                 spiel_beendet = True
                 break
